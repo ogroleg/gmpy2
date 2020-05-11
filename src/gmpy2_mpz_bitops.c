f1321e9b0c08d1459a1430e652fcb6759934e655
Oleg Gromyak <ogroleg@users.noreply.github.com>
2020-05-11 05:06:50 +0300

    Update gmpy2_mpz_bitops.c

--- src/gmpy2_mpz_bitops.c
+++ src/gmpy2_mpz_bitops.c
@@ -659,6 +659,7 @@ GMPy_MPZ_popcount(PyObject *self, PyObject *args)
             Py_DECREF();
             //PyObject *dist = PyLong_FromLong(1);
             PyObject* test = PyDict_GetItem(q, dist);
+            long test2;
             if(test==NULL){test2=0;} else{test2=PyLong_AsLong(test); Py_DECREF(test);}
             PyDict_SetItem(q, dist, PyLong_FromLong(test2+1));
             Py_DECREF(dist);
